9b7c1d45a1a92c0f6fb51951840bfe9b743361cc
ansar <ansar.rnd@gmail.com>
2020-06-19 11:39:09 +0530

    initial commit

---
+++ app/src/main/java/in/techtidbits/viewbinding/MainActivity.kt
@@ -0,0 +1,18 @@
+package `in`.techtidbits.viewbinding
+
+import `in`.techtidbits.viewbinding.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnInfo.setOnClickListener{
+            binding.txtInfo.text = "Button Clicked"
+            Toast.makeText(applicationContext, "Button Clicked", Toast.LENGTH_SHORT ).show()}
+    }
+}
